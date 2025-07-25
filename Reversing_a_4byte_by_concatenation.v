b66923847489cd11c75274c192649c4020fe719e
Abhinav Yadav <153836431+Yabhi07@users.noreply.github.com>
2025-07-25 21:12:08 +0530

    Update Reversing_a_4byte_by_concatenation.v

--- Reversing_a_4byte_by_concatenation.v
+++ Reversing_a_4byte_by_concatenation.v
@@ -1,1 +1,9 @@
+module top_module( 
+  input [31:0] in, // DdddddddAaaaaaaaCcccccccBbbbbbbb
+  output [31:0] out );// BbbbbbbbCcccccccAaaaaaaaDddddddd
+  
+  assign { out[7:0] , out[15:8] , out[23:16] , out[31:24] } =in;
 
+  //by concatenating , swapped the 4 bytes 
+
+endmodule
