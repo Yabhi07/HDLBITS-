d6f15525c9d3cea63c3decc1941b3e8837f37383
Abhinav Yadav <153836431+Yabhi07@users.noreply.github.com>
2025-08-01 20:23:14 +0530

    Create 2 always block2.v

---
+++ Verilog Language /Procedures /2 always block2.v
@@ -0,0 +1,14 @@
+//Instead of the outputs of the blob of logic being visible immediately, the outputs are visible only immediately after the next (posedge clk).
+module top_module(
+    input clk,
+    input a,
+    input b,
+    output wire out_assign,
+    output reg out_always_comb,
+    output reg out_always_ff   ); 
+    
+    assign out_assign = a^b;
+    always @(*) out_always_comb=a^b;
+    always @(posedge clk) out_always_ff=a^b;
+
+endmodule
