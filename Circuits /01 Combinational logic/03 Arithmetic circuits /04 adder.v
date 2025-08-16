b8553feb3a37d270fdee886e1004d275d5ad2852
Abhinav Yadav <153836431+Yabhi07@users.noreply.github.com>
2025-08-16 21:10:04 +0530

    Update 04 adder.v

--- Circuits /01 Combinational logic/03 Arithmetic circuits /04 adder.v
+++ Circuits /01 Combinational logic/03 Arithmetic circuits /04 adder.v
@@ -2,6 +2,7 @@ module top_module (
     input [3:0] x,
     input [3:0] y, 
     output [4:0] sum); 
-     assign sum=x+y;
+     assign sum=x+y; 
+    //sum[4] will act like carry of 4 bit addition 
 
 endmodule 
